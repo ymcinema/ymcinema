789aeb3d9aefdaa0acd47eae32a5d7aa60fa5cdd
Yash Manik <yashmanik36@gmail.com>
2026-04-19 23:01:53 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -20,6 +20,14 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
   });
 
   sources.push({
+    name: "Watch Now 1",
+    key: "Watch Now 1",
+    getMovieUrl: (id) => `https://api.tulnex.com/provider/moviebox/movie/${id}?autoplay=true`,
+    getTVUrl: (id, s, e) => `https://api.tulnex.com/provider/flixhq/tv/${id}/${s}/${e}?autoplay=true`,
+    requiresAuth: false,
+  });
+
+  sources.push({
     name: "Watch Now 2",
     key: "Watch Now 2",
     getMovieUrl: (id) => `https://embed.cinevo.site/movie/${id}?autoplay=true`,
