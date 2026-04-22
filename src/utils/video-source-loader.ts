e99cad49fade754ea94c885af2bac71bce586504
Yash Manik <yashmanik36@gmail.com>
2026-04-22 00:30:41 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -20,14 +20,6 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
   });
 
   sources.push({
-    name: "Watch Now",
-    key: "Watch Now",
-    getMovieUrl: (id) => `https://embed.streammafia.to/embed/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://embed.streammafia.to/embed/tv/${id}/${s}/${e}?autoplay=true`,
-    requiresAuth: false,
-  });
-
-  sources.push({
     name: "Watch Now 2",
     key: "Watch Now 2",
     getMovieUrl: (id) => `https://embed.cinevo.site/movie/${id}?autoplay=true`,
