14a71818c695fa5e86568880e626f6b4138cf31a
Yash Manik <yashmanik36@gmail.com>
2026-04-25 19:03:43 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -20,6 +20,14 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
   });
 
   sources.push({
+    name: "Watch Now",
+    key: "Watch Now",
+    getMovieUrl: (id) => `https://vidzen.fun/movie/${id}?autoplay=true`,
+    getTVUrl: (id, s, e) => `https://vidzen.fun/tv/${id}/${s}/${e}?autoplay=true`,
+    requiresAuth: false,
+  });
+
+  sources.push({
     name: "Watch Now 2",
     key: "Watch Now 2",
     getMovieUrl: (id) => `https://embed.cinevo.site/movie/${id}?autoplay=true`,
