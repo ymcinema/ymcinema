cdfbeefca98a33aee7d03335de823bde6887a7de
Yash Manik <yashmanik36@gmail.com>
2026-06-13 12:43:27 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -12,24 +12,8 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
   const sources: VideoSource[] = [];
 
   sources.push({
-    name: "Watch Now 1",
-    key: "Watch Now 1",
-    getMovieUrl: (id) => `https://player.cinezo.live/embed/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://player.cinezo.live/embed/tv/${id}/${s}/${e}?autoplay=true`,
-    requiresAuth: false,
-  });
-
-  sources.push({
-    name: "Watch Now 2",
-    key: "Watch Now 2",
-    getMovieUrl: (id) => `https://vidzen.fun/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://vidzen.fun/tv/${id}/${s}/${e}?autoplay=true`,
-    requiresAuth: false,
-  });
-
-  sources.push({
-    name: "Watch Now 3",
-    key: "Watch Now 3",
+    name: "Watch Now",
+    key: "Watch Now",
     getMovieUrl: (id) => `https://player.cinezo.live/embed/movie/${id}?autoplay=true`,
     getTVUrl: (id, s, e) => `https://player.cinezo.live/embed/tv/${id}/${s}/${e}?autoplay=true`,
     requiresAuth: false,
