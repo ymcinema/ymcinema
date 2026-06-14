7b1b936607a3ba4fa3fe2101d3800f83d229bbf2
Yash Manik <yashmanik36@gmail.com>
2026-06-14 17:34:47 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -14,8 +14,8 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
   sources.push({
     name: "Watch Now",
     key: "Watch Now",
-    getMovieUrl: (id) => `https://vidfast.pro/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://vidfast.pro/tv/${id}/${s}/${e}?autoplay=true`,
+    getMovieUrl: (id) => `https://rivestream.ru/watch?type=movie/${id}?autoplay=true`,
+    getTVUrl: (id, s, e) => `https://rivestream.ru/watch?type=tv/${id}/${s}/${e}?autoplay=true`,
     requiresAuth: false,
   });
 
