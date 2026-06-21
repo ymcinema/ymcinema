aab7ff6f99a33b77b62d9e9479b9715cf484b263
Yash Manik <yashmanik36@gmail.com>
2026-06-21 18:24:57 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -20,8 +20,8 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
   });
 
   sources.push({
-    name: "Watch Now",
-    key: "Watch Now",
+    name: "Watch Now 1",
+    key: "Watch Now 1",
     getMovieUrl: (id) => `https://www.vidsrc.wtf/2/movie/${id}?autoplay=true`,
     getTVUrl: (id, s, e) => `https://www.vidsrc.wtf/2/tv/${id}/${s}/${e}?autoplay=true`,
     requiresAuth: false,
