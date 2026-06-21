f869ff06bae3eb603d0b109da435b022c786f577
Yash Manik <yashmanik36@gmail.com>
2026-06-21 18:22:39 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -14,16 +14,16 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
   sources.push({
     name: "Watch Now",
     key: "Watch Now",
-    getMovieUrl: (id) => `https://www.vidsrc.wtf/2/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://www.vidsrc.wtf/2/tv/${id}/${s}/${e}?autoplay=true`,
+    getMovieUrl: (id) => `https://cinemaos.tech/movie/watch/${id}?autoplay=true&server=Titan`,
+    getTVUrl: (id, s, e) => `https://cinemaos.tech/tv/watch/${id}?season=${s}&episode=${e}?autoplay=true&embed=true`,
     requiresAuth: false,
   });
 
   sources.push({
-    name: "Watch Now 2",
-    key: "Watch Now 2",
-    getMovieUrl: (id) => `https://cinemaos.tech/movie/watch/${id}?autoplay=true&server=Titan`,
-    getTVUrl: (id, s, e) => `https://cinemaos.tech/tv/watch/${id}?season=${s}&episode=${e}?autoplay=true&embed=true`,
+    name: "Watch Now",
+    key: "Watch Now",
+    getMovieUrl: (id) => `https://www.vidsrc.wtf/2/movie/${id}?autoplay=true`,
+    getTVUrl: (id, s, e) => `https://www.vidsrc.wtf/2/tv/${id}/${s}/${e}?autoplay=true`,
     requiresAuth: false,
   });
 
