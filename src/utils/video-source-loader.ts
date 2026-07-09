7dd8933b03a5087edffb0ddde4ca7cb2abd81b41
Yash Manik <yashmanik36@gmail.com>
2026-07-08 20:06:34 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -38,8 +38,8 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
     sources.push({
     name: "Watch Now 4",
     key: "Watch Now 4",
-    getMovieUrl: (id) => `https://vidbolt.xyz/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://vidbolt.xyz/tv/${id}/${s}/${e}?autoplay=true`,
+    getMovieUrl: (id) => `https://vidup.to/movie/${id}?autoplay=true`,
+    getTVUrl: (id, s, e) => `https://vidup.to/tv/${id}/${s}/${e}?autoplay=true`,
     requiresAuth: false,
   });
 
