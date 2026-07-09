14df11a2eec363f7ec63269166caca527f995c85
Yash Manik <yashmanik36@gmail.com>
2026-07-08 20:12:46 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -38,8 +38,8 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
     sources.push({
     name: "Watch Now 4",
     key: "Watch Now 4",
-    getMovieUrl: (id) => `https://vidup.to/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://vidup.to/tv/${id}/${s}/${e}?autoplay=true`,
+    getMovieUrl: (id) => `https://vidfast.vc/movie/${id}?autoplay=true`,
+    getTVUrl: (id, s, e) => `https://vidfast.vc/tv/${id}/${s}/${e}?autoplay=true`,
     requiresAuth: false,
   });
 
