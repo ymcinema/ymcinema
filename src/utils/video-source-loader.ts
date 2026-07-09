ec46e0bcf62e9b790d8ff8be7958a431de5b58d3
Yash Manik <yashmanik36@gmail.com>
2026-07-08 20:31:12 -0500

    updated provider

--- src/utils/video-source-loader.ts
+++ src/utils/video-source-loader.ts
@@ -38,8 +38,8 @@ export async function fetchVideoSources(): Promise<VideoSource[]> {
     sources.push({
     name: "Watch Now 4",
     key: "Watch Now 4",
-    getMovieUrl: (id) => `https://vidcore.net/movie/${id}?autoplay=true`,
-    getTVUrl: (id, s, e) => `https://vidcore.net/tv/${id}/${s}/${e}?autoplay=true`,
+    getMovieUrl: (id) => `https://www.cineby.at/movie/${id}?autoplay=true`,
+    getTVUrl: (id, s, e) => `https://www.cineby.at/tv/${id}/${s}/${e}?autoplay=true`,
     requiresAuth: false,
   });
 
