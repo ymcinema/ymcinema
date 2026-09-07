63110cf85b044c27e600d6df7b1fdc2687b9ab96
Yash Manik <yashmanik36@gmail.com>
2026-09-07 13:39:18 -0500

    updated provider

--- src/components/player/VideoPlayer.tsx
+++ src/components/player/VideoPlayer.tsx
@@ -60,6 +60,7 @@ const VideoPlayerComponent = ({
         title={title}
         className="h-full w-full"
         allowFullScreen
+        sandbox="allow-scripts allow-same-origin allow-presentation"
         allow="autoplay; encrypted-media; picture-in-picture"
         referrerPolicy="no-referrer"
         loading="lazy"
